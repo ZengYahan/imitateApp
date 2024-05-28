0f7943c284454f6d390b8381fe33b5c487fd2876
Han <g43424140@gmail.com>
2024-05-29 00:27:12 +0800

    Initial Commit

---
+++ imitateApp/imitateAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  imitateAppApp.swift
+//  imitateApp
+//
+//  Created by YaHan on 2024/5/29.
+//
+
+import SwiftUI
+
+@main
+struct imitateAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
