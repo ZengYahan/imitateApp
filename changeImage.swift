40755b3546c0f62fa096fbaea36a483421a4eee2
Han <g43424140@gmail.com>
2024-05-29 16:17:06 +0800

    3

---
+++ changeImage.swift
@@ -0,0 +1,18 @@
+//
+//  changeImage.swift
+//  imitateApp
+//
+//  Created by YaHan on 2024/5/29.
+//
+
+import SwiftUI
+
+struct changeImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    changeImage()
+}
